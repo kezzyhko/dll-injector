586b0c8b0cd93b537e87011ce3bf8a9c03d351c7
Sergey Semushin <s.semushin@innopolis.university>
2021-05-20 23:15:21 +0300

    Remove redundant code from 'app' and 'library'

--- app/Source.cpp
+++ app/Source.cpp
@@ -1,32 +1,5 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
-
-
-bool __declspec(noinline) isPasswordCorrect(string password) {
-	hash<string> hash;
-	return (hash(password) == 1444710931);
-	// If you are curious, it's "pa55w0rd!1"
-	// But shh, I didn't tell you that
-}
-
-
-void __declspec(noinline) givePrize() {
-	cout << "You Won!";
-}
-
+#include <Windows.h>
 
 int main() {
-	cout << "Enter the password: ";
-	string password;
-	cin >> password;
-
-	if (!isPasswordCorrect(password)) {
-		cout << "Wrong password";
-		return -1;
-	}
-
-	givePrize();
-	return 0;
+    MessageBox(nullptr, L"This message is from the app", L"Application", MB_OK);
 }
