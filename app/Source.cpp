c110c53bc6a60ba41c692394d6362649fa5232a2
Sergey Semushin <s.semushin@innopolis.university>
2021-05-20 23:16:25 +0300

    Add x64 support

--- app/Source.cpp
+++ app/Source.cpp
@@ -1,5 +1,11 @@
 #include <Windows.h>
 
+#if defined(_WIN64)
+    constexpr auto TITLE = L"Application x64";
+#else
+    constexpr auto TITLE = L"Application x32";
+#endif
+
 int main() {
-    MessageBox(nullptr, L"This message is from the app", L"Application", MB_OK);
+    MessageBox(nullptr, L"This message is from the app", TITLE, MB_OK);
 }
